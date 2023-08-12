97abc5d7dea3689dd929b755a772a08720bd3c40
hwc <hwc20040629@mail.ustc.edu.cn>
2023-08-12 21:02:58 +0800

    hw7

--- Homework7/Assignment7/Renderer.cpp
+++ Homework7/Assignment7/Renderer.cpp
@@ -5,43 +5,72 @@
 #include <fstream>
 #include "Scene.hpp"
 #include "Renderer.hpp"
+#include <thread>
+#include <atomic>
 
 
 inline float deg2rad(const float& deg) { return deg * M_PI / 180.0; }
 
-const float EPSILON = 0.00001;
+const float EPSILON = 0.001;    // 默认的太小，会导致画面较暗，有黑线
 
 // The main render function. This where we iterate over all pixels in the image,
 // generate primary rays and cast these rays into the scene. The content of the
 // framebuffer is saved to a file.
-void Renderer::Render(const Scene& scene)
-{
+void Renderer::Render(const Scene& scene) {
     std::vector<Vector3f> framebuffer(scene.width * scene.height);
 
     float scale = tan(deg2rad(scene.fov * 0.5));
     float imageAspectRatio = scene.width / (float)scene.height;
     Vector3f eye_pos(278, 273, -800);
-    int m = 0;
 
     // change the spp value to change sample ammount
-    int spp = 16;
+    int spp = 128;
+    int thread_num = 49;
+    std::thread th[thread_num];
+    int per_rows = scene.height / thread_num;
+    std::atomic_int progress = 0;
     std::cout << "SPP: " << spp << "\n";
-    for (uint32_t j = 0; j < scene.height; ++j) {
-        for (uint32_t i = 0; i < scene.width; ++i) {
-            // generate primary ray direction
-            float x = (2 * (i + 0.5) / (float)scene.width - 1) *
-                      imageAspectRatio * scale;
-            float y = (1 - 2 * (j + 0.5) / (float)scene.height) * scale;
+    std::cout << "thread_num: " << thread_num << "\n";
+    auto render_row = [&](uint32_t ymin, uint32_t ymax) {
+        for (uint32_t j = ymin; j < ymax; ++j) {
+            for (uint32_t i = 0; i < scene.width; ++i) {
+                // generate primary ray direction
+                float x = (2 * (i + 0.5) / (float)scene.width - 1) *
+                    imageAspectRatio * scale;
+                float y = (1 - 2 * (j + 0.5) / (float)scene.height) * scale;
 
-            Vector3f dir = normalize(Vector3f(-x, y, 1));
-            for (int k = 0; k < spp; k++){
-                framebuffer[m] += scene.castRay(Ray(eye_pos, dir), 0) / spp;  
+                Vector3f dir = normalize(Vector3f(-x, y, 1));
+                for (int k = 0; k < spp; k++) {
+                    framebuffer[int(j * scene.width + i)] += scene.castRay(Ray(eye_pos, dir), 0) / spp;
+                }
             }
-            m++;
+            progress += 1;
         }
-        UpdateProgress(j / (float)scene.height);
+        UpdateProgress(progress / (float)scene.height);
+        };
+    for (int i = 0; i < thread_num; ++i) {
+        th[i] = std::thread(render_row, i * per_rows, (i + 1) * per_rows);
+    }
+    for (std::thread& x : th) {
+        x.join();
     }
     UpdateProgress(1.f);
+    // for (uint32_t j = 0; j < scene.height; ++j) {
+    //     for (uint32_t i = 0; i < scene.width; ++i) {
+    //         // generate primary ray direction
+    //         float x = (2 * (i + 0.5) / (float)scene.width - 1) *
+    //             imageAspectRatio * scale;
+    //         float y = (1 - 2 * (j + 0.5) / (float)scene.height) * scale;
+
+    //         Vector3f dir = normalize(Vector3f(-x, y, 1));
+    //         for (int k = 0; k < spp; k++) {
+    //             framebuffer[m] += scene.castRay(Ray(eye_pos, dir), 0) / spp;
+    //         }
+    //         m++;
+    //     }
+    //     UpdateProgress(j / (float)scene.height);
+    // }
+    // UpdateProgress(1.f);
 
     // save framebuffer to file
     FILE* fp = fopen("binary.ppm", "wb");
@@ -53,5 +82,5 @@ void Renderer::Render(const Scene& scene)
         color[2] = (unsigned char)(255 * std::pow(clamp(0, 1, framebuffer[i].z), 0.6f));
         fwrite(color, 1, 3, fp);
     }
-    fclose(fp);    
+    fclose(fp);
 }
--- Homework7/Assignment7/global.hpp
+++ Homework7/Assignment7/global.hpp
@@ -9,18 +9,18 @@
 extern const float  EPSILON;
 const float kInfinity = std::numeric_limits<float>::max();
 
-inline float clamp(const float &lo, const float &hi, const float &v)
-{ return std::max(lo, std::min(hi, v)); }
+inline float clamp(const float& lo, const float& hi, const float& v) {
+    return std::max(lo, std::min(hi, v));
+}
 
-inline  bool solveQuadratic(const float &a, const float &b, const float &c, float &x0, float &x1)
-{
+inline  bool solveQuadratic(const float& a, const float& b, const float& c, float& x0, float& x1) {
     float discr = b * b - 4 * a * c;
     if (discr < 0) return false;
-    else if (discr == 0) x0 = x1 = - 0.5 * b / a;
+    else if (discr == 0) x0 = x1 = -0.5 * b / a;
     else {
         float q = (b > 0) ?
-                  -0.5 * (b + sqrt(discr)) :
-                  -0.5 * (b - sqrt(discr));
+            -0.5 * (b + sqrt(discr)) :
+            -0.5 * (b - sqrt(discr));
         x0 = q / a;
         x1 = c / q;
     }
@@ -28,17 +28,15 @@ inline  bool solveQuadratic(const float &a, const float &b, const float &c, floa
     return true;
 }
 
-inline float get_random_float()
-{
-    std::random_device dev;
-    std::mt19937 rng(dev());
-    std::uniform_real_distribution<float> dist(0.f, 1.f); // distribution in range [1, 6]
+inline float get_random_float() {
+    static std::random_device dev;
+    static std::mt19937 rng(dev());
+    static std::uniform_real_distribution<float> dist(0.f, 1.f); // distribution in range [1, 6]
 
     return dist(rng);
 }
 
-inline void UpdateProgress(float progress)
-{
+inline void UpdateProgress(float progress) {
     int barWidth = 70;
 
     std::cout << "[";
